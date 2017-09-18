88efa91834f2f3afc110e432ef37cb2077763834
Zheng <helloworld09@163.com>
2017-09-18 15:10:20 +0800

    Voting ensemble

---
+++ util/ensemble.py
@@ -0,0 +1,27 @@
+import logging
+from collections import defaultdict
+
+def votingEnrich(filenameList):
+    inputFileList = []
+    for filename in filenameList:
+        inputFileList.append(open(filename, 'r', encoding='utf-8'))
+    with open('data/en_test_enrich.txt', 'w', encoding='utf-8') as outputFile:
+        for lines in zip(*inputFileList):
+            lines = list(map(lambda line: line.strip('\n').split('\t'), lines))
+
+            labels = list(map(lambda line: line[2], lines))
+            fd = defaultdict(int)
+            for label in labels:
+                fd[label] += 1
+            votedLabel = max(fd.items(), key=lambda kv: kv[1])[0]
+            votedLine = list(lines[0])
+            votedLine[2] = votedLabel
+            outputFile.write('\t'.join(votedLine) + '\n')
+
+    for inputFile in inputFileList:
+        inputFile.close()
+    logging.info('Voting finish')
+
+
+if __name__ == '__main__':
+    votingEnrich(['data/enrich_0.99870.txt', 'data/enrich_0.99890.txt', 'data/enrich_0.99890.txt'])
